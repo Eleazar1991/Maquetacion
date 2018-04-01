5af9d0589e6bdab4a90fe96d6e9b2950904e267c
Eleazar Pérez Arencibia <eleazar.yio@gmail.com>
2018-04-01 18:47:55 +0100

    Path imagenes

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ var thefoxthings=['PURCHASERS','SUSCRIBERS','FEATURES ADDED','CUSTOMERS','COMMEN
 function myAccFunc() {
     var x = document.getElementById("AccAppend");
     if (x.className.indexOf("sidebar--nonshow") !== -1) {
-        $('#AccAppend').append(`<img src="/images/Rectangle 7.png" alt="Photo sidebar"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`);
+        $('#AccAppend').append(`<img src="/Maquetacion/images/Rectangle 7.png" alt="Photo sidebar"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`);
         x.className = x.className.replace("sidebar--nonshow", "sidebar--show");
-        document.getElementById('image').src = "/images/怀.png";
+        document.getElementById('image').src = "/Maquetacion/images/怀.png";
     }else{
         if(x.className.indexOf("sidebar--show") !== -1){
             $('#AccAppend').empty();
             x.className = x.className.replace("sidebar--show","sidebar--nonshow");
-            document.getElementById('image').src = "/images/瀀.png";
+            document.getElementById('image').src = "/Maquetacion/images/瀀.png";
         }
     }    
 }
