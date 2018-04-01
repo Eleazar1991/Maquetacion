8aac1a70e7cffc586c3414ecff2769313508428c
Eleazar Pérez Arencibia <eleazar.yio@gmail.com>
2018-04-01 14:16:17 +0100

    Maquetación ~ terminada Falta-> Mejorar CSS,         Añadir parte Responsive         Acabar los sidebar         Mejorar Diseño,BEM y medidas para Pixel Perfect

---
+++ main.js
@@ -0,0 +1,34 @@
+var thefoxthings=['PURCHASERS','SUSCRIBERS','FEATURES ADDED','CUSTOMERS','COMMENTS']
+function myAccFunc() {
+    var x = document.getElementById("AccAppend");
+    if (x.className.indexOf("sidebar--nonshow") !== -1) {
+        $('#AccAppend').append(`<img src="/images/Rectangle 7.png" alt="Photo sidebar"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`);
+        x.className = x.className.replace("sidebar--nonshow", "sidebar--show");
+        document.getElementById('image').src = "/images/怀.png";
+    }else{
+        if(x.className.indexOf("sidebar--show") !== -1){
+            $('#AccAppend').empty();
+            x.className = x.className.replace("sidebar--show","sidebar--nonshow");
+            document.getElementById('image').src = "/images/瀀.png";
+        }
+    }    
+}
+
+function addCount(value){
+    var num=$("#AppendCount"+value).text();
+    $('#AppendCount'+value).empty();
+    $("#AppendCount"+value).append((parseInt(num)+1)+`<a onclick="addCount(${value});" >+</a>`);
+}
+
+function incCount(value){
+    var num=$("#Count"+value).text();
+    $('#Count'+value).children("#num").empty();
+    $("#Count"+value).children("#num").append((parseInt(num)+1)+`<a class="view" onclick="incCount(${value});" >+</i></a>`);
+}
+
+$(".main--thefoxthings").hover(function(){
+    $(this).children("div").children("p").children("a").removeClass("view");
+}, function() {
+    $(this).children("div").children("p").children("a").addClass( "view" );
+  });
+
